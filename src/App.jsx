413ccdf0178823dab5eece98e15bdf128124f2c4
clockworkscorpion <faheem.shereef@outlook.com>
2023-06-20 02:12:21 +0530

    Design Base

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import './App.css'
+import "./App.scss";
 
 function App() {
   return (
-    <>Hello
-    </>
-  )
+    <div className="trello-container">
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">Board Bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://dailyepaper.in/wp-content/uploads/2023/02/cropped-DailyEpaper-Logo.png" />
+              Design & Research
+            </li>
+            <li>second</li>
+            <li>third</li>
+            <li>third</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
